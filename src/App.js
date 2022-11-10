2184c184723f524d821a27e7d1d0eb411b6ce354
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-11-10 13:55:28 -0800

    edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect} from "react";
-import { apiKey } from "./config";
+import apiKey  from "./config";
 
 
 import { Routes, Route, Navigate } from "react-router-dom"; 
@@ -11,6 +11,7 @@ import Nav from "./components/Nav";
 import NotFound from "./components/NotFound";
 import PhotoContainer from "./components/PhotoContainer";
 import SearchForm from "./components/SearchForm";
+import { render } from "@testing-library/react";
 
 
 const App = (props) => {
@@ -20,6 +21,7 @@ const App = (props) => {
   const performSearch = (query= '') => { 
 
     useEffect(() => {
+
       fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
         .then((results) => results.json())
         .then(responseData => {
@@ -30,25 +32,28 @@ const App = (props) => {
             } if (query  === 'computers'){
               setcomp(responseData.photos.photo)
           }
+        
+
         });
-
-return (
-  <div className="container">
-    <SearchForm onSearch={performSearch}/>
-      <Nav />
+      
+    }
+    return (
+      <div className="container">
+        <SearchForm onSearch={performSearch}/>
+          <Nav />
     
-      <Routes>
+          <Routes>
 
-          <Route path ="/" element={<Navigate to="/cats" />} />
-          <Route path ="/cats" element={<PhotoContainer data="cats" />} />
-          <Route path ="/dogs" element={<PhotoContainer data="dogs" />} />
-          <Route path ="/computers" element={<PhotoContainer data="computers" />} />
-          <Route path = "*" element={<NotFound />} />
-          <Route path ="/search/:keyword" element={<PhotoContainer />} />
+            <Route path ="/" element={<Navigate to="/cats" />} />
+            <Route path ="/cats" element={<PhotoContainer data="cats" />} />
+            <Route path ="/dogs" element={<PhotoContainer data="dogs" />} />
+            <Route path ="/computers" element={<PhotoContainer data="computers" />} />
+            <Route path = "*" element={<NotFound />} />
+            <Route path ="/search/:keyword" element={<PhotoContainer />} />
 
-      </Routes>
+          </Routes>
     
-  </div>
+     </div>
   )
-
+}}
 export default App;
