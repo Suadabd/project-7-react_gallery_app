5d7ecb5316a2df70ea1696b011b9c0fabe0ef75b
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-11-15 11:06:24 -0800

    fixed app.js & photo container.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import apiKey  from "./config";
 
 import { Routes, Route, Navigate } from "react-router-dom"; 
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+
 
 
 ///App components 
@@ -12,7 +12,7 @@ import Nav from "./components/Nav";
 import NotFound from "./components/NotFound";
 import PhotoContainer from "./components/PhotoContainer";
 import SearchForm from "./components/SearchForm";
-import NoResults from "./components/NoResults";
+
 
 
  
@@ -27,7 +27,6 @@ const App = (props) => {
   //const performSearch = (query= '') => { }
 
 
-  const navigate = useNavigate();
 //state
 const [cats, setCat] = useState([]);
 const [dogs, setDog] = useState([]);
@@ -40,24 +39,23 @@ useEffect(() => {
     
     for (let i in queries){
     
-    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${i}&per_page=24&format=json&nojsoncallback=1`)
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${i}&text=${i}&per_page=24&format=json&nojsoncallback=1`)
       .then((results) => results.json())
       .then(responseData => {
           if(i  ==='cats') {
             setCat(responseData.photos.photo);
             // console.log(responseData);
-            navigate ("/cats");
           } if (i  ==='dogs') {
             setDog(responseData.photos.photo);
           } if (i  === 'computers'){
             setComp(responseData.photos.photo)
         }
-      
     })
-    }},[queries]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }},[]);
 /// [queries] if this var/queries changes call use effect again.
     useEffect(() => {
-      fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${tag}&per_page=24&format=json&nojsoncallback=1`)
+      fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=&tags=${tag}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`)
       .then((results) => results.json())
       .then(responseData => {setPhoto(responseData.photos.photo)})
     },[tag]);
@@ -72,13 +70,12 @@ useEffect(() => {
         <SearchForm onSearch={searchText=> setTag(searchText)}/>
           <Nav />
           <Routes>
-
+            <Route index element={<Navigate to="/cats" replace= {true}/>} />
             <Route path ="/cats" element={<PhotoContainer data={cats} />} />
             <Route path ="/dogs" element={<PhotoContainer data={dogs} />} />
             <Route path ="/computers" element={<PhotoContainer data={computers} />} />
-            <Route path = "*" element={<NotFound />} />
-            <Route path = "/NoResults" element={<NoResults />} />
             <Route path ="/search/:keyword" element={<PhotoContainer data={photo} />} />
+            <Route path = "*" element={<NotFound />} />
 
           </Routes>
       </div>
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Photo from "./Photo";
-// import NoResults from './NoResults';
-import { useNavigate } from 'react-router-dom';
+import NoResults from './NoResults';
 // import NotFound from "./components/NotFound";
 
 
@@ -9,23 +8,9 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-const PhotoContainer = (props) => {
+const PhotoContainer = ({ data }) => {
  
-  const results = props.data; 
-  const navigate = useNavigate();
-  //console.log(results);
-
-  // let photos = results.map( (photo) => 
-  // <Photo url ={`https://farm5.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt="" />
-  // );
-
-  let photos;
-
-  if (results.length > 0 ){
-      photos = results.map( photo => (<Photo url = {`https://farm5.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt=""/>));
-    } else {
-      navigate ("/NoResults");
-    }
+  
 
 
 
@@ -43,7 +28,10 @@ const PhotoContainer = (props) => {
   return (
     <div className="photo-container">
       <h2>Results</h2>
-      <ul> { photos } </ul>
+      <ul>
+        { data.length ? data.map(photo => <Photo url = {`https://farm5.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt="" />)
+          : <NoResults /> }
+      </ul>
     </div>
   )
 
